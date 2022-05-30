d30f8f3fda2761ea7b8adda3b477e9afe700b4dc
lukejacksonn <lukejacksonn@gmail.com>
2022-05-30 23:23:55 +0100

    Fetch pokemons list via gql query

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,12 +1,30 @@
+import { gql } from '@urql/core';
 import { For } from 'solid-js';
-import { pokemonList } from '../data';
+import { createQuery, useClient } from '../utils/solid-urql';
 import PokemonCard from './PokemonCard';
+
+const ALL_POKEMON_QUERY = gql`
+  query Pokemons {
+    pokemons {
+      id
+      name
+    }
+  }
+`;
 
 // TODO: query paginated list
 const PokemonList = () => {
+  const client = useClient();
+  const [query] = createQuery(() => ({
+    client,
+    query: ALL_POKEMON_QUERY,
+  }));
+
+  const pokemons = () => query()?.data.pokemons;
+
   return (
     <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-      <For each={pokemonList}>{(pokemon) => <PokemonCard {...pokemon} />}</For>
+      <For each={pokemons()}>{(pokemon) => <PokemonCard {...pokemon} />}</For>
     </div>
   );
 };
