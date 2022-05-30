d3aea68913cab6d4a27727cd8ee012a728d9639e
lukejacksonn <lukejacksonn@gmail.com>
2022-05-30 23:24:17 +0100

    Add previous and next links to pokemon view

--- src/components/PokemonSprite.tsx
+++ src/components/PokemonSprite.tsx
@@ -1,23 +1,26 @@
 import { css, tw } from 'twind/css';
 
-const PokemonSprite = ({ alt, id }: { alt: string; id: string }) => (
-  <div
-    class={tw(
-      css`
-        & {
-          aspect-ratio: 1 / 1;
-        }
-      `
-    )}
-  >
-    <img
-      alt={alt}
-      class="w-full h-full object-fill"
-      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${Number(
-        id
-      )}.png`}
-    />
-  </div>
-);
+const PokemonSprite = (props: { alt: string; id: string }) => {
+  return (
+    <div
+      class={tw(
+        'flex items-center',
+        css`
+          & {
+            aspect-ratio: 1 / 1.2;
+          }
+        `
+      )}
+    >
+      <img
+        alt={props.alt}
+        class="w-full object-fill"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${Number(
+          props.id
+        )}.png`}
+      />
+    </div>
+  );
+};
 
 export default PokemonSprite;
--- src/pages/outlet/pokemon/[id].tsx
+++ src/pages/outlet/pokemon/[id].tsx
@@ -51,6 +51,11 @@ const PokemonModal = () => {
 
   const pokemon = () => query()?.data?.pokemon;
 
+  const incrementId = (by: number) => {
+    const num = parseInt(pokemon()?.id.replace(/^0+/g, ''));
+    return String(num + by).padStart(3, '0');
+  };
+
   return (
     <div class="flex fixed inset-0 p-5 bg-gray-600 bg-opacity-70 overflow-y-auto h-full w-full">
       <div class="relative m-auto border w-full max-w-[50ch] md:max-w-screen-md shadow-2xl rounded-2xl bg-white">
@@ -58,19 +63,28 @@ const PokemonModal = () => {
           <span class="font-light text-gray-400 text-xl">
             Pokémon #{params.id}
           </span>
-          <Link href="../../" aria-label="Close" class="w-8 h-8 p-2" noScroll>
-            <CloseIcon />
-          </Link>
+          <div class="flex gap-3 children:(w-8 h-8 p-1.5 border rounded-full text-gray-500 fill-current)">
+            <Show when={params.id !== '001'}>
+              <Link href={`../${incrementId(-1)}`} noScroll>
+                <PrevIcon />
+              </Link>
+            </Show>
+            <Show when={params.id !== '151'}>
+              <Link href={`../${incrementId(1)}`}>
+                <NextIcon />
+              </Link>
+            </Show>
+            <Link href="../../" aria-label="Close" noScroll>
+              <CloseIcon />
+            </Link>
+          </div>
         </div>
         <div class="grid md:grid-cols-2">
           <div class="p-8 self-center">
             <PokemonSprite alt={pokemon()?.name || 'Pokémon'} id={params.id} />
           </div>
           <div class="border-t md:border-t-0 md:border-l">
-            <Show
-              when={!query.loading && pokemon()}
-              fallback={<Loader class="opacity-5" />}
-            >
+            <Show when={pokemon()} fallback={<Loader class="opacity-5" />}>
               <div class="flex flex-col h-full relative p-8 gap-4">
                 <div class="flex items-start">
                   <div class="mr-auto">
@@ -98,9 +112,9 @@ const PokemonModal = () => {
                   >
                     {({ name, type, damage }) => (
                       <div class="flex items-center gap-2 text-gray-600 p-1">
-                        <span class="mr-auto text-lg">{name}</span>
+                        <span class="mr-auto">{name}</span>
                         <span class="text-xs">{type}</span>
-                        <span class="text-lg font-bold">{damage}</span>
+                        <span class="font-bold">{damage}</span>
                       </div>
                     )}
                   </For>
@@ -115,15 +129,29 @@ const PokemonModal = () => {
 };
 
 const CloseIcon = () => (
-  <svg
-    class="w-full h-full"
-    stroke-width={4}
-    aria-hidden="true"
-    stroke="currentColor"
-    viewBox="0 0 50 50"
-  >
-    <line x1="50" y2="50" />
-    <line x2="50" y2="50" />
+  <svg class="w-full h-full" viewBox="0 0 16 16">
+    <path
+      fill-rule="evenodd"
+      d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"
+    ></path>
+  </svg>
+);
+
+const NextIcon = () => (
+  <svg class="w-full h-full" viewBox="0 0 16 16">
+    <path
+      fill-rule="evenodd"
+      d="M8.22 2.97a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06l2.97-2.97H3.75a.75.75 0 010-1.5h7.44L8.22 4.03a.75.75 0 010-1.06z"
+    ></path>
+  </svg>
+);
+
+const PrevIcon = () => (
+  <svg class="w-full h-full" viewBox="0 0 16 16">
+    <path
+      fill-rule="evenodd"
+      d="M7.78 12.53a.75.75 0 01-1.06 0L2.47 8.28a.75.75 0 010-1.06l4.25-4.25a.75.75 0 011.06 1.06L4.81 7h7.44a.75.75 0 010 1.5H4.81l2.97 2.97a.75.75 0 010 1.06z"
+    ></path>
   </svg>
 );
 
