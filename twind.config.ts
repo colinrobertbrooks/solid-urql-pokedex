4afc734f2cf03f721751d5de123a0d5a137807c3
lukejacksonn <lukejacksonn@gmail.com>
2022-05-30 20:41:16 +0100

    Add responsive base font size

--- twind.config.ts
+++ twind.config.ts
@@ -1,6 +1,15 @@
 import { Configuration } from 'twind';
+import { css } from 'twind/css';
 
 export const config: Configuration = {
+  preflight: (defaults) => css`
+    ${defaults};
+    html,
+    body {
+      font-size: clamp(14px, 2vmin, 18px);
+      overscroll-behavior-y: none;
+    }
+  `,
   theme: {
     extend: {},
   },
