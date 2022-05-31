22583e83cc5a809f4adf68d0947e3581782b60ae
lukejacksonn <lukejacksonn@gmail.com>
2022-05-31 22:15:39 +0100

    Fix overflow scroll in the x axis

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,25 +4,29 @@ import { PokemonLogo } from './PokemonLogo';
 // backdrop-filter: blur(10px);
 
 const Navigation = () => (
-  <nav class={tw('flex items-center flex-wrap p-4 sticky top-0 mb-12 z-10')}>
-    <div
+  <nav class={tw('sticky top-0 z-10 mb-6 flex')}>
+    <svg
+      viewBox="0 0 981 86"
+      width="100%"
+      height="6rem"
+      preserveAspectRatio="none"
       class={tw(
-        'absolute w-[110vw] min-w-[980px] transform left-1/2 -translate-x-[50%]',
         css`
-          & svg {
+          & {
             filter: drop-shadow(0 0 1rem rgb(0 0 0 / 0.2));
             fill: #fff;
           }
         `
       )}
     >
-      <svg class="w-full" viewBox="0 0 981 86">
-        <path d="M0.714294 68.3651V17.3651C0.714294 7.97621 8.32543 0.365051 17.7143 0.365051H963.714C973.103 0.365051 980.714 7.97621 980.714 17.365V68.3651C980.714 77.7539 973.103 85.3651 963.714 85.3651H692.18C689.818 85.3651 687.483 84.8731 685.322 83.9204L644.76 66.038C642.599 65.0854 640.264 64.5934 637.902 64.5934H319.968C317.607 64.5934 315.271 65.0854 313.111 66.038L272.549 83.9204C270.388 84.8731 268.052 85.3651 265.691 85.3651H17.7143C8.32545 85.3651 0.714294 77.7539 0.714294 68.3651Z" />
-      </svg>
-    </div>
+      <path
+        d="M0.714294 85.3651V0.365051H980.714V85.3651H751.018C749.414 85.3651 747.82 85.1239 746.288 84.6498L683.795 65.3087C682.263 64.8345 680.668 64.5934 679.064 64.5934H278.807C277.203 64.5934 275.608 64.8345 274.076 65.3087L211.583 84.6498C210.051 85.1239 208.456 85.3651 206.853 85.3651H0.714294Z"
+        fill="white"
+      />
+    </svg>
     <div
       class={tw(
-        'mx-auto -mb-14 max-w-[62%]',
+        'absolute top-4 w-1/6 min-w-[16rem] w-24 max-w-[50%] left-1/2 transform -translate-x-1/2',
         css`
           & svg {
             filter: drop-shadow(0 0.3rem 0.2rem rgb(0 0 0 / 0.4));
